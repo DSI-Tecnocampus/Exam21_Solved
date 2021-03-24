31bbd56594337b52b84978072e0f5e527d606d49
Josep Roure Alcobé <roure@tecnocampus.cat>
2021-03-24 23:03:00 +0100

    added discovery (with loadbalanced) and gateway

--- notes/src/main/java/cat/tecnocampus/notes/usersAdapter/UserExistsAdapter.java
+++ notes/src/main/java/cat/tecnocampus/notes/usersAdapter/UserExistsAdapter.java
@@ -2,6 +2,7 @@ package cat.tecnocampus.notes.usersAdapter;
 
 import cat.tecnocampus.notes.application.portsOut.CallUserExists;
 import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Component;
@@ -11,11 +12,14 @@ import org.springframework.web.client.RestTemplate;
 public class UserExistsAdapter implements CallUserExists {
     private final RestTemplate restTemplate;
     private final CircuitBreakerFactory circuitBreakerFactory;
+    private final String userServiceUrl;
 
 
-    public UserExistsAdapter(RestTemplate restTemplate, CircuitBreakerFactory circuitBreakerFactory) {
+    public UserExistsAdapter(RestTemplate restTemplate, CircuitBreakerFactory circuitBreakerFactory,
+                             @Value("${app.users-service.host}") String usersServiceHost) {
         this.restTemplate = restTemplate;
         this.circuitBreakerFactory = circuitBreakerFactory;
+        this.userServiceUrl = "http://" + usersServiceHost + "/users/exists/";
     }
 
     @Override
@@ -23,7 +27,7 @@ public class UserExistsAdapter implements CallUserExists {
     public String userExists(String userName) {
         CircuitBreaker circuitBreaker = circuitBreakerFactory.create("user");
         return circuitBreaker.run(
-                () -> restTemplate.getForObject("http://localhost:8080/users/exists/" + userName, String.class),
+                () -> restTemplate.getForObject(userServiceUrl + userName, String.class),
                 throwable -> {
                     System.out.println(throwable.getMessage());
                     return "open";});
